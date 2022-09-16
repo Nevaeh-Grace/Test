c22bab41fae0995edb1175e253dcd3af1953afa5
SAPARK <seulae46@naver.com>
2022-09-16 15:32:46 +0900

    20220916

--- 1-2.js
+++ 1-2.js
@@ -1,7 +1,16 @@
 function solution(a,b,c){
-    let answer;
+    let answer = "yes";
+    let max;
+    let sum = a + b + c;
+
+    if (a > b) max = a;
+    else max = b;
+
+    if (answer < c) max = c;
+
+    if ((sum - max) <= max) answer = "no";
 
     return answer;
 }
 
-console.log(solution(6,7,11));
+console.log(solution(11,6,7));
